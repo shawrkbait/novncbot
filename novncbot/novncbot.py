4391b00a690499deb661393468eb9cd6ad579845
Garrett Perkins <gperkins@caci.com>
2020-01-15 14:45:45 -0600

    Add driver creation via string or custom driver

--- novncbot/novncbot.py
+++ novncbot/novncbot.py
@@ -2,7 +2,9 @@
 from enum import IntEnum
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import Options as firefoxOptions
+from selenium.webdriver.chrome.options import Options as chromeOptions
+from selenium.webdriver.remote.webdriver import WebDriver
 import logging
 
 log = logging.getLogger(__name__)
@@ -18,12 +20,28 @@ class Keys(IntEnum):
 class NoVNCbot(object):
 
   def __init__(self, driver=None):
-    options = Options()
-    options.headless = True
-    if driver is not None:
+    '''
+    Instantiate NoVNCbot for interacting with a VNC Console
+    @param driver: 			Optional override that accepts the following values
+    					String: firefox, ff  -- Creates Firefox browser in headless mode
+					String: chrome  -- Creates chrome browser in headless mode
+					WebDriver:	-- Uses webdriver  created by caller
+					None:	Creates Firefox browser in headless mode
+    '''
+    if isinstance(driver,WebDriver):
         self.driver = driver
+    elif isinstance(driver,str) and  ( driver.lower() == 'firefox' or driver.lower() == 'ff'):
+        firefox_options = firefoxOptions()
+        firefox_options.headless = True
+        self.driver = webdriver.Firefox(options = firefox_options)
+    elif isinstance(driver,str) and  ( driver.lower() == 'chrome'):
+        chrome_options = chromeOptions()
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(options=chrome_options)
     else:
-        self.driver = webdriver.Firefox(options=options)
+        default_options = firefoxOptions()
+        default_options.headless = True
+        self.driver = webdriver.Firefox(optionsi = default_options)
 
   def __sendKey(self, keycode):
     log.debug("pressing %s" % hex(keycode))
