37e77e5e616d8529cce92574698be5053a3f9a88
shawrkbait <sjohnson@axiomega.com>
2020-01-16 13:38:53 -0600

    Bump version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="novncbot",
-    version="0.0.1",
+    version="0.1.0",
     author="Shawn Johnson",
     author_email="sjohnson@axiomega.com",
     description="An asynchronous noVNC client",
